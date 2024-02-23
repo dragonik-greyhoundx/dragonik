7868e20f125712abf43fbf31cd46bbc22e241a47
guillaume <guillaume.douchement@gmail.com>
2024-02-23 12:21:16 +0100

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 // Dependencies
-import { useState } from 'react';
+import { useState, useEffect, ReactNode } from 'react';
 // Css
 import './app.css';
 // Components
 import TopBar from './components/TopBar';
 import BodyView from './components/BodyView';
 import FootBar from './components/FootBar';
+// Pages
+import Home from './pages/home';
 
 export default function App () {
     // States
-    const [onPage, setOnPage] = useState<string>("");
+    const [onPage, setOnPage] = useState<ReactNode>(null);
+    // Effects
+    useEffect(() => {
+        if (onPage === null) {
+            setOnPage( <Home /> );
+        }
+    }, []);
     // Render
     return (
         <div id="app">
             <TopBar />
-            <BodyView />
+            <BodyView children={onPage} />
             <FootBar />
         </div>
     );
--- src/components/BodyView/index.tsx
+++ src/components/BodyView/index.tsx
@@ -1,15 +1,21 @@
 // Dependencies
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 // Css
 import './index.css';
+// Interfaces
+interface BodyViewProps {
+    children: ReactNode,
+};
 
-export default function BodyView () {
+export default function BodyView (props: BodyViewProps) {
+    // Props
+    const { children } = props;
     // States
     const [displayType, setDisplayType] = useState<string>("");
     // Render
     return (
         <div id="bodyview">
-            
+            {children}
         </div>
     );
 };
