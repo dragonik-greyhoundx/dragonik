728eeb0cc14178b8a8cbdd06b3b21806ebb2e0d2
guillaume <guillaume.douchement@gmail.com>
2024-02-23 10:54:11 +0100

    Adding home

---
+++ src/pages/home.tsx
@@ -0,0 +1,15 @@
+// Dependencies
+import { useState } from 'react';
+// Css
+import './home.css';
+
+export default function Home () {
+    // States
+    const [isDisplay, setIsDisplay] = useState<boolean>(false);
+    // Render
+    return (
+        <div id="home">
+            
+        </div>
+    );
+}
